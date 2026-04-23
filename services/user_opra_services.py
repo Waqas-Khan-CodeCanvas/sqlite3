bc43ee397cc283a09eed7e5038f825bed8d8c7f4
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-04-23 23:43:43 +0500

    feat:(get by name) function add for database operation

---
+++ services/user_opra_services.py
@@ -0,0 +1,23 @@
+
+
+from repository.user_repository import UserRepository
+from config.logger import Logger
+from database.db import db
+
+log = Logger("User-Services")
+User = UserRepository()
+
+class UserServices:
+    
+    def get_by_username(self , username):
+        
+        if not username:
+            raise Exception("username required.")
+        
+        user = User.get_by_username(username)
+        if not user:
+            raise Exception("user not found.")
+        
+        return user   
+        
+    
