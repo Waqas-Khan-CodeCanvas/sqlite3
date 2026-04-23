45abf396a416d3737b5619b4e78ad37ceb39547c
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-04-23 23:44:33 +0500

    feat:(get by id) function add for database operation

--- services/user_opra_services.py
+++ services/user_opra_services.py
@@ -9,15 +9,23 @@ User = UserRepository()
 
 class UserServices:
     
-    def get_by_username(self , username):
-        
+    def get_by_username(self , username):    
         if not username:
             raise Exception("username required.")
         
         user = User.get_by_username(username)
         if not user:
             raise Exception("user not found.")
-        
-        return user   
-        
     
+        return user   
+    
+    def get_by_userId(self, id):
+        if not id:
+            raise Exception("username required.")
+        
+        user = User.get_by_userId(id)
+        if not user:
+            raise Exception("user not found.")
+        
+        return user 
+  
